77d521d35f2b0486f70b80772c1c28282aacf6a7
Maicon Silva <maiconrs95@gmail.com>
2020-07-12 19:19:07 -0300

    feat: add method to render album info

---
+++ src/AlbumInfo.js
@@ -0,0 +1,10 @@
+export default function renderAlbumInfo(album, element) {
+    const markup = `
+        <img class="album-image" src="${album.images[0].url}" alt="${album.name}">
+        <p class="album-title">${album.name}</p>
+        <p class="album-artist">${album.artists[0].name}</p>
+        <p class="album-counter">${album.tracks.total} Músicas</p>
+    `;
+
+    element.innerHTML = markup;
+}
