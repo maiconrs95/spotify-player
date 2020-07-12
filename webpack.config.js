0781c2fb560a91f7ba40c35f5984a84ab524e018
Maicon Silva <maiconrs95@gmail.com>
2020-07-12 01:24:10 -0300

    feat: webpack server config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    context: path.resolve(__dirname, './src'),
+    entry: {
+        app: './main.js',
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './app')
+    },
+    devServer: {
+        contentBase: path.join(__dirname, './app'),
+        compress: true,
+        port: 8000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/],
+                use: 'babel-loader',
+            },
+        ],
+    },
+    plugins: [
+        new webpack.SourceMapDevToolPlugin({
+            exclude: [/node_modules/],
+        }),
+    ]
+};
