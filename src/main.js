6964a17f6b2ff4650b64724bdfc43d7d97e58e19
Maicon Silva <maiconrs95@gmail.com>
2020-07-12 19:03:49 -0300

    feat: add album infor tests

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Spotify from './spotify';
 import renderAlbums from './Album';
 
 (async () => {
-    const { albums } = await Spotify.search.albums('red-hot-chili-peppers');
-    const albumListEL = document.getElementById('album-list');
+    try {
+        const { albums } = await Spotify.search.albums('red-hot-chili-peppers');
+        const albumListEL = document.getElementById('album-list');
 
-    renderAlbums(albums.items, albumListEL);
+        renderAlbums(albums.items, albumListEL);
+    } catch (e) {}
 })();
