0222826eaf7e35f48a5bc4f0da4eec337090c9a6
Maicon Silva <maiconrs95@gmail.com>
2020-07-12 13:38:16 -0300

    feat: render albums tests

--- src/Album.js
+++ src/Album.js
@@ -1,3 +1,16 @@
+function creatreMarkup(data) {
+    return data.map((album) => `
+        <div class="list-item">
+            <img src="${album.images[2].url}" alt="${album.name}" class="list-image">
+            <div class="list-description">
+                <p class="list-title">${album.name}</p>
+                <p class="list-subtitle">${album.artists[0].name}</p>
+            </div>
+        </div>`).join('');
+}
+
 export default function renderAlbums(data, element) {
-    return null;
+    const markup = creatreMarkup(data);
+
+    element.innerHTML = markup;
 }
