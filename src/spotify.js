556b78aff844346299a6e634970c426e6866969f
Maicon Silva <maiconrs95@gmail.com>
2020-07-12 03:11:45 -0300

    add: spotify sdk to project

---
+++ src/spotify.js
@@ -0,0 +1,7 @@
+import SpotifyWrapper from 'spotify-sdk-wrapper';
+
+const spotify = new SpotifyWrapper({
+    token: 'BQAhipGZyCkTOXBT43KbWdzYjzG_vGJJ4khgeUco4XPN62kF8bM29603gxQgfPI5bnXbCUT7MRFa0aWdK_bU2QyotBmoirFUg6vzMXjCo4a_OFrCvzNx1dfqslTiWk3iZkp4rEK3SLJexQ',
+});
+
+export default spotify;
--- tests/Spotify.spec.js
+++ tests/Spotify.spec.js
@@ -1,3 +1,12 @@
 import { expect } from 'chai';
+import spotify from '../src/spotify';
 
-describe('Spotify', () => {});
+describe('Spotify', () => {
+    it('should be an object', () => {
+        expect(spotify).to.be.an('object');
+    });
+
+    it('should be seatch methods', () => {
+        expect(spotify.search).to.exist;
+    });
+});
