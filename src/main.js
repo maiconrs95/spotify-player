7d8e566af8fca36fb0974a140d751acaba103889
Maicon Silva <maiconrs95@gmail.com>
2020-07-13 01:00:09 -0300

    add: playlist trigger

--- src/main.js
+++ src/main.js
@@ -1,19 +1,7 @@
-import Spotify from './spotify';
 import searchEnterTrigger from './SearchTrigger';
 import SelectAlbumTrigger from './SelectAlbumTrigger';
+import playlistTrigger from './PlaylistTrigger';
 
 searchEnterTrigger();
 SelectAlbumTrigger();
-
-// (async () => {
-//     try {
-//         const albumInfoEL = document.getElementById('album-info');
-//         const albumTracksEL = document.getElementById('album-tracks');
-//         const data = await Spotify.album.getAlbum('2Y9IRtehByVkegoD7TcLfi');
-
-//         const albumData = renderAlbumInfo(data, albumInfoEL);
-//         renderAlbumTracks(albumData.tracks.items, albumTracksEL);
-//     } catch (e) {
-//         console.log(e);
-//     }
-// })();
+playlistTrigger();
